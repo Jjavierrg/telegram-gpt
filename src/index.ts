0a60777d72b2719ae32c5be9ac9c01aeeed2b845
José Javier Rodríguez Gallego <jjavierrg@gmail.com>
2023-04-21 14:14:21 +0200

    refactor: :recycle: change to grammy client

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,20 @@
-import TelegramBot from 'node-telegram-bot-api';
-import { Configuration, OpenAIApi } from 'openai';
-const configuration = new Configuration({ organization: process.env.OPENAI_ORGANIZATION!, apiKey: process.env.OPENAI_API_KEY! });
-const openai = new OpenAIApi(configuration);
-
 // replace the value below with the Telegram token you receive from @BotFather
+import { Bot } from 'grammy';
+
 const token = process.env.TELEGRAM_TOKEN!;
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true, onlyFirstMatch: true });
+// Create a bot using the Telegram token
+const bot = new Bot(token);
 
-bot.on('message', (msg) => console.log(msg));
-
-// Matches /start
-bot.onText(/\/start/, (msg) => {
-  const greeting = `Hey ${msg.chat.first_name}, this is Chat GPT! \n\nI'm a bot powered with the most advanced artificial intelligence available. \n\nHow can I help you?`;
-  bot.sendMessage(msg.chat.id, greeting);
+bot.command('start', (ctx) => {
+  const greeting = `Hey ${ctx.from?.first_name}, this is Chat GPT! \n\nI'm a bot powered with the most advanced artificial intelligence available. \n\nHow can I help you?`;
+  ctx.reply(greeting);
 });
 
-// handles the rest of the text messages
-bot.onText(/(.+)/, async (msg) => {
-  const chatId = msg.chat.id;
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [{ role: 'user', content: msg.text! }]
-  });
-
-  bot.sendMessage(chatId, response.data.choices[0].message?.content ?? "I don't know what to say");
+bot.on('message:text', (ctx) => {
+  const message = ctx.message?.text ?? 'No message received';
+  console.log(message);
+  ctx.reply(message);
 });
+
+bot.start();
